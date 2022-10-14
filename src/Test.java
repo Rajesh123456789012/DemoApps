52af87b2ad4b53ad3d2213a9af70bf808dc907c9
Tarunraj101422 <tharunraj313@gmail.com>
2022-10-14 08:09:17 +0530

    added subtract method

--- src/Test.java
+++ src/Test.java
@@ -4,8 +4,13 @@ public class Test {
         System.out.println("2nd commit update.");
 
         add(10,20);
+        sub(20,10);
     }
     public static void add(int a, int b){
         System.out.println(a+b);
+    }
+
+    public static void sub(int x, int y){
+        System.out.println(x-y);
     }
 }
