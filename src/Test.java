b8e9784b778c6e9488d2a361211361a1381fa61a
swathiannam <annamswa3@gmail.com>
2022-10-14 07:40:09 +0530

    added new method

--- src/Test.java
+++ src/Test.java
@@ -2,5 +2,9 @@ public class Test {
     public static void main(String[] args){
         System.out.println("test");
         System.out.println("2nd commit update.");
+        add(10,20);
+    }
+    public static void add(int a, int b){
+        System.out.println(a+b);
     }
 }
