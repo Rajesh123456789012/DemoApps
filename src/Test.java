51fb2d8855707e538f5346da32b35ffc0a8462cb
Tarunraj101422 <tharunraj313@gmail.com>
2022-10-19 11:22:32 +0530

    added div method

--- src/Test.java
+++ src/Test.java
@@ -6,6 +6,7 @@ public class Test {
         add(10,20);
         sub(20,10);
         multiplication(2,3);
+        Test.div(10,2);
     }
     public static void add(int a, int b){
         System.out.println(a+b);
@@ -16,5 +17,10 @@ public class Test {
     }
     public static void multiplication(int m,int n){
         System.out.println(m*n);
+    }
+
+    public static void div(int d, int e){
+        int g = d/e;
+        System.out.println("Answer is " +g);
     }
 }
