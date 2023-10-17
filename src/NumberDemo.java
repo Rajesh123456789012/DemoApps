e680c5e838eff5f92dc26cdedbef5ea77538a811
Rajesh123 <rajeshhelpss@gmail.com>
2023-10-17 08:22:20 +0530

    added even number logic

--- src/NumberDemo.java
+++ src/NumberDemo.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
-
 public class NumberDemo {
+    public static void main(String[] args){
+
+        int n =11;
+
+        if(n%2 == 0)
+            System.out.println("its a even number:"+n);
+        else
+            System.out.println("its an odd number:"+n);
+    }
+
 }
